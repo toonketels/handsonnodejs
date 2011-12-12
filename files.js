dd74badd8563fd807403674e5752b888b6118f62
Toon Ketels <toon@puresign.be>
2011-12-12 20:15:15 +0100

    File system exercise 2 - pg. 55

--- files.js
+++ files.js
@@ -11,3 +11,24 @@ fs.stat( 'a.txt', function( error, stats ) {
     // Call the size property of stats object
     console.log( stats.size );
 });
+
+// Open file
+fs.open( 'a.txt', 'r', function( error, fd ) {
+    // Once opened, check error first
+    if ( error ) {
+        console.log( error.message );
+        return;
+    }
+    // Create buffer and read file next
+    var buffer = new Buffer( 5 );
+    // We read just part of file and drop content in buffer
+    fs.read( fd, buffer, 0, 5, 10, function( error, bytesRead, buffer ) {
+        // Once read, handle error first
+        if ( error ) {
+            console.log( error.message );
+            return;
+        }
+        // Log the items saved in buffer
+        console.log( buffer.toString() );
+    });
+});
