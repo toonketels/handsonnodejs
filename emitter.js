59923204e45b6722a258164f24cc4482dc6ca618
Toon Ketels <toon@puresign.be>
2011-12-12 18:55:16 +0100

    EventEmitter exercise 2 - pg.44

--- emitter.js
+++ emitter.js
@@ -12,15 +12,22 @@ var Ticker = function() {
     // Wouldn't be possible with self
     var self = this;
     setInterval( function() {
+        // Though we haven't inherited from the EventEmitter object yet,
+        // (and as a consequence, we don't have a emit method),
+        // we do can call this method because we are inheriting a bit below.
         self.emit( 'tick' );
     }, 1000);
 }
 
+// Actual subclass from EventEmitter
 util.inherits( Ticker, EventEmitter );
 
 
-// new
+
+// My object instance
 var myTicker = new Ticker();
+// We add a listener, this will execute the anonymous function
+// that logs TIC
 myTicker.addListener( 'tick', function(){
     console.log( 'TICK' );
 });
