9f711e9a1c923f84cda67e8f55a9904c25cac9cb
Toon Ketels <toon@puresign.be>
2011-12-12 21:50:56 +0100

    File system exercise 6 - pg. 56
    Changing a char.

--- files.js
+++ files.js
@@ -1,26 +1,36 @@
-// We are going to append a string.
+// We are going to change a char
 
 var fs = require( 'fs' );
 
 // flow:
 // open it
-// write to it (no need to read it)
+// create buffer
+// append buffer at location
 
-// Open a file to be appended to (a flag)
-fs.open( 'b.txt', 'a', function( error, fileDescription ){
+
+// Open a file with 'a' flag 
+fs.open( 'b.txt', 'a', function( error, fd ){
     if ( error ) {
-        throw( error );
+        throw error;
     }
-    // Create a buffer
-    // Passing utf8 as encoding is not necesary as it's the defaults for strings.
-    var buffer = new Buffer('abc', 'utf8');
-    // Write to the file.
-    // Passing null as position of file will append the string
-    // on the current possition (and we opened file with a flag so its at the end)
-    fs.write( fileDescription, buffer, 0, buffer.length, null, function( error, written, buffer ) {
-        if ( error ) {
-            throw( error );
-        }
-        console.log( 'Written to file: ' + buffer.toString() )
-    });
+    
+    // Create buffer
+    var buffer = new Buffer( '7' );
+    var written = 0;
+    
+    // Write to the file
+    (function changeChar() {
+        fs.write( fd, buffer, written, buffer.length - written, 10, function( error, bytesWritten, buffer ){
+            if ( error ) {
+                throw error;
+            }
+            written += bytesWritten;
+            if (written === buffer.length) {
+                console.log( 'We have done it!' );
+            } else {
+                changeChar();
+                console.log( 'Go again' );
+            }
+        });
+    })();
 });
