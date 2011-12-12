1937b8db4298acf405de04d82e383723b26bd26d
Toon Ketels <toon@puresign.be>
2011-12-12 20:51:52 +0100

    File sytem exercise 3 - pg. 55
    Declaring functions which pass arguments to callbacks being passed to it is so cool!

--- files.js
+++ files.js
@@ -1,16 +1,4 @@
-// We need the file system module for low level file operations.
 var fs = require( 'fs' );
-
-// Stat method to get some info about the file
-fs.stat( 'a.txt', function( error, stats ) {
-    // Log the error if we have one
-    if ( error ) {
-        console.log( error.message );
-        return;
-    }
-    // Call the size property of stats object
-    console.log( stats.size );
-});
 
 // Open file
 fs.open( 'a.txt', 'r', function( error, fd ) {
@@ -19,16 +7,50 @@ fs.open( 'a.txt', 'r', function( error, fd ) {
         console.log( error.message );
         return;
     }
-    // Create buffer and read file next
-    var buffer = new Buffer( 5 );
-    // We read just part of file and drop content in buffer
-    fs.read( fd, buffer, 0, 5, 10, function( error, bytesRead, buffer ) {
-        // Once read, handle error first
-        if ( error ) {
-            console.log( error.message );
-            return;
-        }
-        // Log the items saved in buffer
-        console.log( buffer.toString() );
+    // Since we are going to execute the same functionality twice,
+    // wrap it in a function. Here we declare this function.
+    // The only things differnet is the startingpoint, possibly the lenght
+    // and the buffer to print.
+    function readSome( startingAt, byteCount, callback ) {
+        var buffer = new Buffer( byteCount );
+        // Set readBytes to 0, this var is going to keep track how for
+        // we have read into buffer
+        var readBytes = 0;
+        // We create a self executing function because when we check if we
+        // were successful and we find out not all bytes have been read, we
+        // are going te execute this function again
+        (function readIt() {
+            // Standard fs.read method
+            fs.read( fd, buffer, readBytes, buffer.length - readBytes, startingAt + readBytes, function( error, bytesRead, buffer ) {
+                if ( error ) {
+                    console.log( error.message );
+                    return;
+                }
+                // Set readBytes to the number of bytes actually read.
+                // This is our additional check to be sure we have read it all.
+                readBytes += bytesRead;
+                // If we have read it all, execute the callback var (function) provided
+                // by the caller and pass the buffer along.
+                if (bytesRead === buffer.length) {
+                    callback(buffer);
+                // If we haven't read the entire length, try to read it again from
+                // where we ended.
+                } else {
+                    readIt();
+                }
+            
+            });
+        })();
+    }
+    // Execute the function we just declared.
+    // Our callback gets the buffer argument
+    readSome( 5, 4, function( buffer1 ){
+        // We log the output and execute the function
+        // again in our callback
+        console.log( buffer1.toString() );
+        readSome( 9, 4, function( buffer2 ){
+            // Log the second buffer
+            console.log( buffer2.toString() );
+        });
     });
 });
