ebcb6773a876d29488201b19b45d4395826a51cc
Toon Ketels <toon@puresign.be>
2011-12-13 18:48:31 +0100

    Performing http requests example - pg. 62
    By passing options to http request method and write data and end the request, we actually
    perform a request.
    
    Note difference between:
    	- request event emitted by server
    	- http.request method to perform requests

--- server.js
+++ server.js
@@ -1,9 +1,29 @@
-// Add util module for its inspect method,
-// usefull for debugging.
-var util = require( 'util' );
+// Create the options we want to pass in the request
+var options = {
+    host: 'www.google.com',
+    port: 80,
+    path: '/upload',
+    method: 'POST'
+}
 
-require('http').createServer(function(request, response){
-    response.writeHead(200, {'Content-Type': 'text/plain'});
-    // Inspect the request headers object.
-    response.end( util.inspect(request.headers) );
-}).listen(4000);
+// We peform a request with the options,
+// and execute the callback witht the response object when
+// we get a response.
+var request = require( 'http' ).request( options, function( response ){
+    // We log the some data about the response we get back
+    console.log( 'STATUS: ' + response.statusCode );
+    console.log( 'HEADERS: ' + JSON.stringify( response.headers ));
+    // Set an encoding so we get a string instead of a buffer
+    response.setEncoding( 'utf8' );
+    // For every data 'package' we get back, log the contents
+    // note: the 'data' event was emitted 5 times.
+    // We are streaming data from the server
+    response.on( 'data', function( chunck ){
+        console.log( 'BODY: ' + chunck );
+    });
+});
+
+// Here we actually performing the request to google.
+request.write( "data\n" );
+request.write( "data\n" );
+request.end();
